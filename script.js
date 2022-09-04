96bf203535bb52f16bb4520cb78f1f2be945a4db
Pacifique HAKIZIMANA <57889075+paccy123456789@users.noreply.github.com>
2022-09-04 03:10:04 -0700

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Front-end Developer", "Back-End Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Front-end Developer", "Back-End Developer","Blogger", "Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Front-end Developper","Back-End Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Front-end Developper","Back-End Developer", "Blogger", "Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
